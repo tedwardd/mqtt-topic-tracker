6324650957efa3b27911972faac5d66e4c93a662
Edward Wood <ted_wood@inuit.com>
2024-11-19 14:59:01 -0500

    Fix frontend (TODO: fix db arg)

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -11,23 +11,23 @@ import (
 	"github.com/tedwardd/mqtt-topic-tracker/internal/topics"
 )
 
-var DbConn *string
+var DbPath *string
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	var loggedTopics []topics.Topic
 
-	conn := *DbConn
-	dbconn, err := sql.Open("sqlite3", conn)
+	dbconn, err := sql.Open("sqlite3", "/mqtt_data/topics.db")
 	if err != nil {
 		panic(err)
 	}
+	defer dbconn.Close()
 
 	query := `SELECT * from topics ORDER BY count DESC`
 	rows, err := dbconn.Query(query)
 	if err != nil {
-		defer rows.Close()
 		panic(err)
 	}
+	defer rows.Close()
 
 	for rows.Next() {
 		var topic topics.Topic
@@ -43,10 +43,11 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	// json, err := json.Marshal(loggedTopics)
 	// fmt.Fprintf(w, "%v", json)
+	return
 }
 
 func main() {
-	DbConn = flag.String("db", "topics.db", "Path of db file")
+	DbPath = flag.String("db", "topics.db", "Path of db file")
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
