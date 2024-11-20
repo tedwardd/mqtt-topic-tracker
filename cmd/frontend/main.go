00932ba77920ff834b75546b1db2b86f68df5eca
Edward Wood <ted_wood@inuit.com>
2024-11-20 14:38:38 -0500

    Update frontend to run with a template for somewhat more "style"

--- cmd/frontend/main.go
+++ cmd/frontend/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/tedwardd/mqtt-topic-tracker/internal/topics"
@@ -13,36 +13,55 @@ import (
 
 var DbPath *string
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	var loggedTopics []topics.Topic
+type TopicPageData struct {
+	PageTitle string
+	Topics    []topics.Topic
+}
 
-	dbconn, err := sql.Open("sqlite3", "/mqtt_data/topics.db")
+func handler(w http.ResponseWriter, r *http.Request) {
+	data := TopicPageData{
+		PageTitle: "Topic Traffic",
+		Topics:    []topics.Topic{},
+	}
+
+	dbconn, err := sql.Open("sqlite3", "topics.db")
 	if err != nil {
 		panic(err)
 	}
+
 	defer dbconn.Close()
 
 	query := `SELECT * from topics ORDER BY count DESC`
+
 	rows, err := dbconn.Query(query)
 	if err != nil {
 		panic(err)
 	}
+
 	defer rows.Close()
+	var topic topics.Topic
+
+	tmpl := template.New("topics")
 
 	for rows.Next() {
-		var topic topics.Topic
+
 		if err := rows.Scan(&topic.Name, &topic.Count); err != nil {
 			panic(err)
 		}
-		fmt.Fprintf(w, "%s - %d\n", topic.Name, topic.Count)
-		loggedTopics = append(loggedTopics, topic)
+
+		// fmt.Fprintf(w, "%s - %d\n", topic.Name, topic.Count)
+		tmpl, err = template.ParseFiles("web/templates/mqtt_stats.gohtml")
+		if err != nil {
+			panic(err)
+		}
+
+		data.Topics = append(data.Topics, topic)
 		if err = rows.Err(); err != nil {
 			panic(err)
 		}
-	}
 
-	// json, err := json.Marshal(loggedTopics)
-	// fmt.Fprintf(w, "%v", json)
+	}
+	tmpl.Execute(w, data)
 	return
 }
 
